847ffd1e0b67bfbfeb9d4ad5d82eb5380addc715
Atte Dilip <attedilip2121999@gmail.com>
2022-07-21 07:23:22 +0530

    changes

--- tests/myprograms/src/validatemail.java
+++ tests/myprograms/src/validatemail.java
@@ -1,11 +1,27 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class validatemail 
 {
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
-		
-
+		ArrayList<String> emails=new ArrayList<String>();
+		emails.add("Diilip@gmail.com");
+		emails.add("ram@gmail.com");
+		emails.add("ravi@gmail.com");
+		emails.add("harsha@gmail.com");
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter your email");
+		String id=sc.nextLine();
+		if(emails.contains(id))
+		{
+			System.out.println("your employee emailid is successfully found\n"+id);
+		}
+		else
+		{
+			System.out.println("your employee email.id is not found");
+		}
 	}
 
 }
